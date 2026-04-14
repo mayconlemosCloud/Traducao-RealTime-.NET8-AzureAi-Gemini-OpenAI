c0d756aa6f9fc71375c8b1a4112e022332d3329c
mayconlemosCloud <mayconlemos.riosaude@gmail.com>
2026-04-14 11:41:17 -0300

    refactor: remove MeetingGoogle project from solution

--- MeetingGoogle/Models/AudioDeviceInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace MeetingGoogle.Models
-{
-    public class AudioDeviceInfo
-    {
-        public int DeviceIndex { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
